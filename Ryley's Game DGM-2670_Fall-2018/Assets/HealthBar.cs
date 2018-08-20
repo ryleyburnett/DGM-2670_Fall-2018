9af3d4463d72aaff233c910f22069c2a41b2edd0
Ryley Burnett <ryleyburnett@live.com>
2018-08-20 14:24:32 -0600

    Added Scripts to Unity 1
    1

---
+++ Ryley's Game DGM-2670_Fall-2018/Assets/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+	public floatdata Health;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		
+		
+		
+	}
+}
---
+++ Ryley's Game DGM-2670_Fall-2018/Assets/IfStatements.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+
+	
+	}public float Speed;
+	public bool LightsOn;
+
+	// Use this for initialization
+	void Start () {
+		if (!LightsOn || Speed > 4)
+		{
+			//do work
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+}
