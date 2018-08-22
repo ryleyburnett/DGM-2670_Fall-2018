7f103971738b8ddea9272f08ab42d1b9cf800ea2
Ryley Burnett <ryleyburnett@live.com>
2018-08-22 14:28:22 -0600

    Ugly Dog Unity 2670

---
+++ Ryley's Game DGM-2670_Fall-2018/Assets/PrefabData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+//Made by Ryley Burnett
+[CreateAssetMenu]
+public class PrefabData : ScriptableObject
+{
+    public GameObject Prefab;
+    public Sprite SpriteImage;
+    public Color SpriteColor;
+
+    public void OnConfigure()
+    {
+        var newPrefab = Instantiate(Prefab);
+        var newSprite = newPrefab.GetComponent<SpriteRenderer>();
+        newSprite.sprite = SpriteImage;
+        newSprite.color = SpriteColor;
+    }
+
+}
