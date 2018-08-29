8fbe81b38049b244c9774c5f5cc1c919d3e372ab
Ryley Burnett <ryleyburnett@live.com>
2018-08-29 14:46:41 -0600

    Added Bear, Robot, Bolt, etc

--- Ryley's Game DGM-2670_Fall-2018/Assets/EventScript.cs
+++ Ryley's Game DGM-2670_Fall-2018/Assets/EventScript.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class EventScript : MonoBehaviour {
+public class EventScript : MonoBehaviour 
+{
+	public UnityEvent Event;
 
-	// Use this for initialization
-	void Start () {
-		
+	private void OnMouseDown()
+	{
+		Event.Invoke();
 	}
+
+		
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
