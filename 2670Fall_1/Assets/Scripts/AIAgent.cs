2d5e135d401a01e33eddcbde5cad197b4b15e127
Ryley Burnett <ryleyburnett@live.com>
2018-09-17 17:12:27 -0600

    Crashed Proj
    but also finished uvs for general

---
+++ 2670Fall_1/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AIAgent : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	public Transform Destination;
+	public Transform PostPoint;
+	private Transform finalDestination;
+	
+	private void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		finalDestination = transform;
+	}
+
+	private void OnTriggerEnter(Collider obj)
+	{
+		if(obj.transform == Destination)
+		finalDestination = Destination;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		finalDestination = PostPoint;
+	}
+
+	private void Update()
+
+	{
+		agent.destination = finalDestination.position;
+		
+		
+	}
+
+}
