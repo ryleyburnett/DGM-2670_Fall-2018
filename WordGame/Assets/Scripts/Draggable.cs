8dbf091beb82726e503bf9a4c4b5959ba338c6c8
Ryley Burnett <ryleyburnett@live.com>
2018-10-01 19:56:42 -0600

    Bolt Teleport, Death, Etc. Also Cartoon finishing

---
+++ WordGame/Assets/Scripts/Draggable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+
+	private Vector3 currentPosition;
+	private Vector3 newPosition;
+	private Camera cam;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		cam = Camera.main;
+
+	}
+
+	private void OnMouseDown()
+	{
+		currentPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	// Update is called once per frame
+	void OnMouseDrag()
+	{
+		newPosition = currentPosition + cam.ScreenToWorldPoint(Input.mousePosition);
+		newPosition.z = 0;
+		transform.position = newPosition;
+	}
+}
---
+++ WordGame/Assets/Scripts/TriggerID.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerID : MonoBehaviour
+{
+    public NameID ID;
+    public UnityEvent OnMatch;
+    
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (ID == obj.GetComponent<ObjectID>().ID)
+        {
+         OnMatch.Invoke();   
+            print("Match");
+        }
+    }
+
+
+}
