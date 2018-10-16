d79f781ece6598b5fb861a21bfa11f355fc7c649
Ryley Burnett <ryleyburnett@live.com>
2018-10-15 19:15:25 -0600

    Graphics and Bolt and Scripting and Arm

---
+++ WordGame/Assets/Store/StoreScripts/Objects.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Object", menuName = "Store/Objects0")]
+public class Objects : ScriptableObject
+{
+    public List<Object> ObjectList;
+  
+}
---
+++ WordGame/Assets/Store/StoreScripts/Store.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Store", menuName = "Store/StoreFront")]
+public class Store : ScriptableObject
+{
+    public Objects Available;
+    public Objects Purchased;
+    
+    public void MakePurchase(Object obj)
+    {
+        for (var i = 0; i < Available.ObjectList.Count; i++)
+        {
+            var availableObject = Available.ObjectList[i];
+            if (availableObject == obj)
+            {
+                Purchased.ObjectList.Add(obj);
+                Available.ObjectList.Remove(availableObject);
+            }
+        }
+    }
+}
