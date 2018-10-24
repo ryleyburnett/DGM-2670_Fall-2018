73f25b420debac797a62964109283581df2cf897
Ryley Burnett <ryleyburnett@live.com>
2018-10-24 14:54:58 -0600

    WalkyFish

---
+++ FishProjectSchoolComp/Assets/Scripts/IsDraggable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class IsDraggable : MonoBehaviour
+{
+
+	private Vector3 offsetPosition;
+	private Vector3 newPosition;
+	private Camera cam;
+
+	public bool CanDrag;
+
+	private void Start()
+	{
+		cam = Camera.main;
+	}
+
+	private IEnumerator OnMouseDown()
+	{
+		offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+		yield return new WaitForFixedUpdate();
+		CanDrag = true;
+
+		while (CanDrag)
+		{
+			yield return new WaitForFixedUpdate();
+			newPosition = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
+			transform.position = newPosition;
+		}
+	}
+
+	private void OnMouseUp()
+	{
+		CanDrag = false;
+	}
+	
+}
