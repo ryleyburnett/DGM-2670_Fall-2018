8cabe61b768f042a6a7eb16ab1aff4086aa58d8d
Ryley Burnett <ryleyburnett@live.com>
2018-10-29 16:23:04 -0600

    Changes to Git

---
+++ FishProjectSchoolComp/Assets/Scripts/ParentGround.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class ParentGround : MonoBehaviour {
+
+	public GameObject ParentObj;
+	public GameObject Parented;
+	public Vector3 ParentPos;
+	public Transform objectA;
+	public Transform objectB;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		print("Collided");
+		
+		//Make ObjectA's position match objectB
+		objectA.position = objectB.position;
+   
+		//Now parent the object so it is always there
+		objectA.parent = objectB;
+			
+		//ParentObj.transform.parent = Parented.transform;
+		
+	}
+
+	
+}
+
